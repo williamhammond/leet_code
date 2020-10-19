cdcad8ff744c82c1de9f9d28cf94d22ce4018771
William Hammond <william.t.hammond@gmail.com>
2020-10-18 20:38:26 -0400

    Working implementation of fancy sequence in rust

--- src/main.rs
+++ src/main.rs
@@ -2,49 +2,89 @@ mod tests;
 
 use std::cell::RefCell;
 
-const MOD: i32 = 1000000007;
+const MOD: usize = 1000000007;
 
 fn main() {
 
 }
 
-
+#[derive(Debug)]
 struct Fancy {
-    seq: RefCell<Vec<i32>>,
+    seq: RefCell<Vec<(usize, usize, usize)>>,
+    inc: usize,
+    mult: usize,
 }
 
-/**
- * `&self` means the method takes an immutable reference.
- * If you need a mutable reference, change it to `&mut self` instead.
- */
 impl Fancy {
 
     fn new() -> Self {
         return Fancy{
             seq: RefCell::new(Vec::new()),
+            inc: 0,
+            mult: 1,
         };
     }
 
     fn append(&self, val: i32) {
-        self.seq.borrow_mut().push(val);
+        self.seq.borrow_mut().push((val as usize, self.inc, self.mult));
     }
 
-    fn add_all(&self, inc: i32) {
-        for i in self.seq.borrow_mut().iter_mut() {
-            *i += inc;
-        }
+    fn add_all(&mut self, inc: i32) {
+        self.inc += inc as usize;
     }
 
-    fn mult_all(&self, m: i32) {
-        for i in self.seq.borrow_mut().iter_mut() {
-            *i *= m;
-        }
+    fn mult_all(&mut self, m: i32) {
+        self.mult = (self.mult * m as usize) % MOD;
+        self.inc = (self.inc * m as usize) % MOD;
     }
 
     fn get_index(&self, idx: i32) -> i32 {
         if idx as usize >= self.seq.borrow_mut().len() {
             return -1;
         }
-        return self.seq.borrow_mut()[idx as usize] % MOD;
+        let (v, v_inc, v_mult) = self.seq.borrow()[idx as usize];
+        let ratio = (self.mult * self.pow_mod(v_mult, MOD - 2, MOD)) % MOD;
+
+        return (((ratio * (MOD + v - v_inc)) + self.inc) % MOD) as i32;
+    }
+
+    fn pow_mod(&self, base: usize, exp: usize, modulo: usize) -> usize {
+        // Sanity check:  Verify that the exponent is not negative:
+        assert!(exp >= 0);
+
+        if exp == 0 {
+            return 1;
+        }
+
+        // Now do the modular exponentiation algorithm:
+        let mut base = base % modulo;
+        let mut exp = exp;
+
+        let mut result = 1;
+        while exp > 0 {
+            if (exp & 1) != 0 {
+                result = (self.mod_mult(result, base, MOD)) % modulo;
+            }
+            base = self.mod_mult(base, base, MOD) % modulo;
+            exp = exp >> 1;
+        }
+
+        return result;
+    }
+
+    fn mod_mult(&self, a: usize , b: usize, m: usize) -> usize {
+        let mut a= a;
+        let mut b = b;
+
+        let mut result = 0;
+        while a != 0 {
+            if (a & 1) != 0 {
+                result = (result + b) % m;
+            }
+            a >>= 1;
+            b = (b << 1) % m;
+        }
+
+        return result;
     }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,14 +1,34 @@
 #[cfg(test)]
 mod tests {
     use crate::Fancy;
+
+    #[test]
+    fn it_pow_mod() {
+        let mut fancy = Fancy::new();
+        let result = fancy.pow_mod(10, 2, 1000);
+        assert_eq!(result, 100);
+    }
+
+    #[test]
+    fn it_handles_single() {
+        let mut fancy = Fancy::new();
+        fancy.append(2);
+        fancy.add_all(3);
+        assert_eq!(fancy.get_index(0), 5);
+
+        fancy.mult_all(10);
+        assert_eq!(fancy.get_index(0), 50);
+    }
+
     #[test]
     fn it_handles_basic() {
-        let fancy = Fancy::new();
+        let mut fancy = Fancy::new();
 
         fancy.append(2);   // fancy sequence: [2]
         fancy.add_all(3);   // fancy sequence: [2+3] -> [5]
         fancy.append(7);   // fancy sequence: [5, 7]
         fancy.mult_all(2);  // fancy sequence: [5*2, 7*2] -> [10, 14]
+
         assert_eq!(fancy.get_index(0), 10); // return 10
 
         fancy.add_all(3);   // fancy sequence: [10+3, 14+3] -> [13, 17]
@@ -22,7 +42,7 @@ mod tests {
 
     #[test]
     fn it_handles_big_number() {
-        let fancy = Fancy::new();
+        let mut fancy = Fancy::new();
 
         fancy.append(12);
         fancy.append(8);
